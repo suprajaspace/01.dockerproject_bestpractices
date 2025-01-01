7a0610028c5250dd79e8787f75f10d62b9fe37c2
Supraja <supra556699@gmail.com>
2025-01-01 15:54:13 +0800

    docker

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-sudo yum install -y yum-utils
-sudo yum config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y docker-compose-plugin -y
-sudo systemctl start docker
-sudo usermod -aG docker ec2-user
-sudo docker ps
-
-echo "docker install success"
-
 df -hT
 lsblk
 sudo growpart /dev/nvme0n1 4
@@ -17,3 +8,14 @@ sudo xfs_growfs /
 sudo xfs_growfs /var
 
 echo "resize EBS success"
+
+
+sudo yum install -y yum-utils
+sudo yum config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y docker-compose-plugin -y
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+sudo docker ps
+
+echo "docker install success"
+
